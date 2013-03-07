8d0210b85c23a1875e3bc5d82bc5d220727fb7f3
davesann <davesann.p@gmail.com>
2013-03-08 01:08:51 +1100

    Update core.cljs - correct doc string, removed unnecessary comment

--- src/cljs_uuid/core.cljs
+++ src/cljs_uuid/core.cljs
@@ -8,13 +8,12 @@
   like: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
   as per http://www.ietf.org/rfc/rfc4122.txt.
   Usage:
-  (UUIDv4)  =>  #uuid \"305e764d-b451-47ae-a90d-5db782ac1f2e\"
-  (type (UUIDv4)) => cljs.core/UUID"
+  (make-random)  =>  #uuid \"305e764d-b451-47ae-a90d-5db782ac1f2e\"
+  (type (make-random)) => cljs.core/UUID"
   []
   (letfn [(f [] (.toString (rand-int 16) 16))
           (g [] (.toString  (bit-or 0x8 (bit-and 0x3 (rand-int 15))) 16))]
     (UUID. (.toString
-             ;(.append 
              (goog.string.StringBuffer.
                (f) (f) (f) (f) (f) (f) (f) (f) "-" (f) (f) (f) (f) 
                "-4" (f) (f) (f) "-" (g) (f) (f) (f) "-"
